6b7b7779265f61494a325190e4d8cce359a9379f
rootrsk <ravishankar7050@gmail.com>
2020-07-07 00:26:58 +0530

    idk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom'
 import App from './components/App'
 import store from './store/store'
+import * as serviceWorker from './serviceWorker';
+
 
 
 
