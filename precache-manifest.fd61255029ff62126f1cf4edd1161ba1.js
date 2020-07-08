ca5c8c29ad31fe573d756590bd51a7d1f70106cf
rootrsk <ravishankar7050@gmail.com>
2020-07-08 16:07:49 +0530

    Updates

---
+++ precache-manifest.fd61255029ff62126f1cf4edd1161ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5616552e3cfc508cdfcf40bf72c1e943",
+    "url": "/gamerangers/index.html"
+  },
+  {
+    "revision": "cdc7ddc93cf0a4d2dfda",
+    "url": "/gamerangers/static/css/main.87843395.chunk.css"
+  },
+  {
+    "revision": "34da597da05b54a0c6aa",
+    "url": "/gamerangers/static/js/2.85c88fb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gamerangers/static/js/2.85c88fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc7ddc93cf0a4d2dfda",
+    "url": "/gamerangers/static/js/main.fc381047.chunk.js"
+  },
+  {
+    "revision": "e7aa787e2e58998f5c11",
+    "url": "/gamerangers/static/js/runtime-main.6bc4a064.js"
+  }
+]);
