82c7697a86dc414b19b75d4a83e63c1d37f7a666
rootrsk <ravishankar7050@gmail.com>
2020-07-11 11:01:34 +0530

    Updates

---
+++ precache-manifest.403165e1393efb07ff77418d1dbfa0d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb2e19357ef4d954dc7f32a6f27b206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "437f17c9b1ca7d088ab1",
+    "url": "/static/css/main.ece1f553.chunk.css"
+  },
+  {
+    "revision": "34da597da05b54a0c6aa",
+    "url": "/static/js/2.85c88fb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.85c88fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437f17c9b1ca7d088ab1",
+    "url": "/static/js/main.b0a8b728.chunk.js"
+  },
+  {
+    "revision": "58684c78d25389b06733",
+    "url": "/static/js/runtime-main.65f878dc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gamerangers/precache-manifest.16b2990e0936b1bad4ea0df86c570266.js"
+  "/precache-manifest.403165e1393efb07ff77418d1dbfa0d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gamerangers/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
